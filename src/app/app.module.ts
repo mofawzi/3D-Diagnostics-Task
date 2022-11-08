4279d8c5a236285a6cab7e1df1a958a4a5fcfe3b
PeOmniBk <71118479+PeOmniBk@users.noreply.github.com>
2022-11-08 21:07:18 +0200

    Add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
@@ -12,6 +13,7 @@ import { ProductsComponent } from './components/products/products.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
