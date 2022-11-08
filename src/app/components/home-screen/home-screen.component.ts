d81169af3ac69da3ac50eefc9d311ac30f0bcfc1
PeOmniBk <71118479+PeOmniBk@users.noreply.github.com>
2022-11-08 23:22:58 +0200

    Update user data

--- src/app/components/home-screen/home-screen.component.ts
+++ src/app/components/home-screen/home-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersServiceService } from './../../services/users-service.service';
 
 @Component({
   selector: 'app-home-screen',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-screen.component.css']
 })
 export class HomeScreenComponent implements OnInit {
-
-  constructor() { }
+  user: any;
+  constructor(private usersService: UsersServiceService) { }
 
   ngOnInit(): void {
+
+    this.usersService.getSingleUser()
+      .subscribe(res => {
+        this.user = res;
+        console.log(res);
+      });
   }
 
 }
